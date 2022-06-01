ec44be8501078eb3f2d595e51fc6de93d30b7213
NumiBlue <thekindlyblog@gmail.com>
2022-06-01 13:13:36 -0400

    adds LGBTQowned copy

--- my-app/src/components/LGBTQowned.jsx
+++ my-app/src/components/LGBTQowned.jsx
@@ -7,6 +7,7 @@ const LGBTQowned = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#99B898] border-[#E84A5F]'>LGBTQ Owned</p>
+                <p>As Pride Month approaches, let us remember that Austin is one of the top LGBTQA+-friendly cities in the U.S. year-round! Our city is full of shops, restaurants, and talented artists, musicians, and artisans that are all part of the community. Here are some examples for businesses and events to support.</p>
                 <p className='py-6 text-[#E84A5F]'>text</p>
            </div>
         </div>
