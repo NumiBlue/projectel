7b0cbf7e1c9700b3cae22c7e8fd213b3d81c9449
NumiBlue <thekindlyblog@gmail.com>
2022-06-01 13:14:49 -0400

    adds taco truck copy

--- my-app/src/components/TacoTrucks.jsx
+++ my-app/src/components/TacoTrucks.jsx
@@ -8,6 +8,7 @@ const TacoTrucks = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#99B898] border-[#E84A5F]'>Tacos</p>
+                <p>If Texas is known for anything, it's TexMex. In a beautiful example of urban symbiosis, Austin's taco trucks display the glory that occurs when TexMex and food truck culture collide! Allow your cravings to guide you through the city in search of the perfect taco; our site will be your lodestar to the port of tasty taco goodness.</p>
                 <p className='py-6 text-[#E84A5F]'>
                 <GiTacos size={30} />
                 <GiTacos size={30} />
