bd1a24d94024306bb01eb7a416e4aa5be548fd4f
NumiBlue <thekindlyblog@gmail.com>
2022-06-01 13:11:05 -0400

    adds craftbeer copy

--- my-app/src/components/CraftBeer.jsx
+++ my-app/src/components/CraftBeer.jsx
@@ -7,6 +7,7 @@ const CraftBeer = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#99B898] border-[#E84A5F]'>Craft Beer</p>
+                <p> After a long day of music festivals, wandering through the Green Belt, or a rough day at the office, it's a good time to check out Austin's reknowned local brewery scene. Austin has a long history of musing up some of the most unusual, innovative, and satisfying brews in the world. Served in a quirky pub, a relaxed outdoor lounge, a high-luxury speak-easy, or during an intense board-gaming session, these libations really hit the spot.</p>
                 <p className='py-6 text-[#E84A5F]'>text</p>
            </div>
         </div>
