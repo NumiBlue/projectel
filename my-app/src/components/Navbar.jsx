b76d4e586807841a5efbee8c9c9a6015275edb8f
Damaris Palacios <dslpz728@gmail.com>
2022-06-01 18:28:52 -0500

    adds cards

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -7,10 +7,12 @@ import FarmersMarkets from './FarmersMarkets'
 import PetFriendly from './PetFriendly'
 import CraftBeer from './CraftBeer'
 
+
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleNav= () => {
       setNav(!nav)
+      
     }
 
   return (
@@ -29,7 +31,7 @@ const Navbar = () => {
             <li><Link to="events" smooth={true} duration={500}>
             Events
             </Link></li>
-            <li><Link to="/CraftBeer" exact component={CraftBeer}></Link>CraftBeer</li>
+            <div><Link to="/CraftBeer" exact component={CraftBeer}></Link>CraftBeer</div>
             <div><Link to="/PetFriendly" exact component={PetFriendly}></Link>PetFriendly</div>
             <div><Link to="/FarmersMarkets" exact component={FarmersMarkets}></Link>FarmersMarkets</div>
           </ul>
