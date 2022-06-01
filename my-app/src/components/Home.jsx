1d3b6369bea01a8eb39696f34f8a8ba0b1b1b478
Damaris Palacios <dslpz728@gmail.com>
2022-06-01 14:58:10 -0500

    adds landing page

---
+++ my-app/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Weather from './Weather';
+import Footer from './Footer';
+import Events from './Events';
+
+
+function Home () {
+  return (
+    
+    <div>
+      <Navbar />
+      <Hero />
+      <Weather />
+      <Events />
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default Home
