e07bb1bd19b6a484034586eb63b72ceda71169a0
NumiBlue <thekindlyblog@gmail.com>
2022-06-01 13:14:13 -0400

    adds pet-friendly copy

--- my-app/src/components/PetFriendly.jsx
+++ my-app/src/components/PetFriendly.jsx
@@ -7,6 +7,7 @@ const PetFriendly = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#99B898] border-[#E84A5F]'>Pet Friendly</p>
+                <p>Pack up your furry friends and go adventuring! The Austin area is full of pet-friendly establishments and events. Well-behaved pets are welcome in spaces all over the area. Between hiking, swimming, and hanging out at bars and cafes, your pet if bound to sleep well at the end of the day.</p>
                 <p className='py-6 text-[#E84A5F]'>text</p>
            </div>
         </div>
