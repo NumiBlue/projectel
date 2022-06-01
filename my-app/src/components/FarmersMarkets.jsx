805fef2b9e759d6afcbb52cd36ce87efe1f5e8d8
NumiBlue <thekindlyblog@gmail.com>
2022-06-01 13:11:53 -0400

    adds farmers market copy

--- my-app/src/components/FarmersMarkets.jsx
+++ my-app/src/components/FarmersMarkets.jsx
@@ -7,6 +7,7 @@ const FarmersMarkets = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#99B898] border-[#E84A5F]'>Farmers Markets</p>
+                <p>Looking for fresh kale? A mushrom log on which to grow your own lion's mane? What about a home-brewed elderberry syrup? We've got you covered. The Austin area has an almost inexhaustable list of farmer's markets and local farms. Some Farmer's Markets are long-running, weekly affairs, and some are pop-ups and collectives between local farmers.Head on over to one of these for your weekly produce or some home-made ginger-beer.</p>
                 <p className='py-6 text-[#E84A5F]'>text</p>
            </div>
         </div>
